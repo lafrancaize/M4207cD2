be5b2a429649102eda14be4ba84cb26aa3dd0f32
thiaw <thiawfatou156@gmail.com>
2022-02-08 11:48:31 +0100

    validité de l'utilisatueur

--- src/Controller/ServeurController.php
+++ src/Controller/ServeurController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\NomClasseTable
+
 
 class ServeurController extends AbstractController
 {
@@ -27,12 +30,36 @@ class ServeurController extends AbstractController
        //Récupère les informations de login
        $Login=$request->request->get("pseudo");
        $password=$request->request->get("password");
+
+       if (($Login =="root") && ($password == "toor"))
+       {
+        $txt = "valide!";
+       }
+        else{
+            $txt = "Non valide!";
+        }
        
         return $this->render('serveur/traitement.html.twig', [
             'controller_name' => 'ServeurController',
             'login' => $Login,
             'password' => $password,
+            'txt' => $txt,
+        
+
+            //Recherche de l'utilisateur dans la base de données
+            $Utilisateur = $manager -> getRepository(Utilisateur)::findOneByLoginpassword$Login$password
+
+            // vérification du mot de passe
+            if ($utilisateur == null) {
+                return new Response ("Utilisateur").$Login.$password"):
+            }
+
+           
         ]);
+    
+
+  
     }
+
 
 }
